6560a5a6770fed6d02fc82c7998ffb88362e172a
Wouter <woutermalan7909@gmail.com>
2022-06-09 11:54:40 +0200

    added flag add the end of each heading

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ let randomImage2 = "/images/dice" + randomNumber2 + ".png";
 document.querySelector(".img2").setAttribute("src", randomImage2);
 
 if(randomNumber1 < randomNumber2){
-  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins! 🚩";
 }
 else{
-    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+    document.querySelector("h1").innerHTML = "Draw!";
 }
