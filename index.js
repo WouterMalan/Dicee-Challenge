d7a3824ac6d52460adff3074cc409f672bce8b20
Wouter <woutermalan7909@gmail.com>
2022-06-09 11:50:02 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;// 1-6 random number
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;// 1-6 random number
+
+//pick out a random image from the 6 images
+let randomImage = "/images/dice" + randomNumber1 + ".png";
+document.querySelector(".img1").setAttribute("src", randomImage);
+
+let randomImage2 = "/images/dice" + randomNumber2 + ".png";
+document.querySelector(".img2").setAttribute("src", randomImage2);
+
+if(randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+else{
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
